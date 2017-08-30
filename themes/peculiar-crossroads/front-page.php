140b4d8265323ee94684f37113b29756c1b4fbab
robotnicka <sheswithoutwings@gmail.com>
2017-08-30 03:14:28 +0000

    Page content and triptych styles

--- themes/peculiar-crossroads/front-page.php
+++ themes/peculiar-crossroads/front-page.php
@@ -43,46 +43,41 @@ get_header();
     				</div>
     			</a>
 			</div>
-		</div>
 
-	    <div class="triptych triptych__container">
 			<div class="triptych__card">
 			    <a href="#">
-    				<div class="triptych__image">
-    					<img src="http://via.placeholder.com/350x150"/>
-    				</div>
-    				<div class="triptych__content">
-    				    <h2>Nyarlathotep... the crawling chaos... </h2>
-    					<p>I do not recall distinctly when it began, but it was months ago. The general tension was horrible. To a season of political and social upheaval was added a strange and brooding apprehension of hideous physical danger; a danger widespread and all-embracing, such a danger as may be imagined only in the most terrible phantasms of the night.</p>
-    				</div>
-    				<div class="triptych__footer">
-    					<a href="#">
-    					    <span class="triptych__dagger">†</span> Read more...
-    					</a>
-    				</div>
-    			</a>
+					<div class="triptych__image">
+						<img src="http://via.placeholder.com/350x150"/>
+					</div>
+					<div class="triptych__content">
+					    <h2>Nyarlathotep... the crawling chaos... </h2>
+						<p>I do not recall distinctly when it began, but it was months ago. The general tension was horrible. To a season of political and social upheaval was added a strange and brooding apprehension of hideous physical danger; a danger widespread and all-embracing, such a danger as may be imagined only in the most terrible phantasms of the night.</p>
+					</div>
+					<div class="triptych__footer">
+						<a href="#">
+						    <span class="triptych__dagger">†</span> Read more...
+						</a>
+					</div>
+				</a>
 			</div>
-		</div>
 
-		<div class="triptych triptych__container">
 			<div class="triptych__card">
 			    <a href="#">
-    				<div class="triptych__image">
-    					<img src="http://via.placeholder.com/350x150"/>
-    				</div>
-    				<div class="triptych__content">
-    				    <h2>Nyarlathotep... the crawling chaos... </h2>
-    					<p>I do not recall distinctly when it began, but it was months ago. The general tension was horrible. To a season of political and social upheaval was added a strange and brooding apprehension of hideous physical danger; a danger widespread and all-embracing, such a danger as may be imagined only in the most terrible phantasms of the night.</p>
-    				</div>
-    				<div class="triptych__footer">
-    					<a href="#">
-    					    <span class="triptych__dagger">†</span> Read more...
-    					</a>
-    				</div>
-    			</a>
+					<div class="triptych__image">
+						<img src="http://via.placeholder.com/350x150"/>
+					</div>
+					<div class="triptych__content">
+					    <h2>Nyarlathotep... the crawling chaos... </h2>
+						<p>I do not recall distinctly when it began, but it was months ago. The general tension was horrible. To a season of political and social upheaval was added a strange and brooding apprehension of hideous physical danger; a danger widespread and all-embracing, such a danger as may be imagined only in the most terrible phantasms of the night.</p>
+					</div>
+					<div class="triptych__footer">
+						<a href="#">
+						    <span class="triptych__dagger">†</span> Read more...
+						</a>
+					</div>
+				</a>
 			</div>
 		</div>
-
 	</div>
 </div>
 
--- themes/peculiar-crossroads/sidebar.php
+++ themes/peculiar-crossroads/sidebar.php
@@ -14,25 +14,22 @@ if ( ! is_active_sidebar( 'sidebar-1' ) ) {
 
 <aside id="secondary" class="widget-area">
 	<div class="featured-event__container">
-		<div class="triptych triptych__container">
-			<div class="triptych__card">
-			    <a href="#">
-    				<div class="triptych__image">
-    					<img src="http://via.placeholder.com/350x150"/>
-    				</div>
-    				<div class="triptych__content">
-    				    <h2>Nyarlathotep... the crawling chaos... </h2>
-    					<p>I do not recall distinctly when it began, but it was months ago. The general tension was horrible. To a season of political and social upheaval was added a strange and brooding apprehension of hideous physical danger; a danger widespread and all-embracing, such a danger as may be imagined only in the most terrible phantasms of the night.</p>
-    				</div>
-    				<div class="triptych__footer">
-    					<a href="#">
-    					    <span class="triptych__dagger">†</span> Read more...
-    					</a>
-    				</div>
-    			</a>
-			</div>
+		<div class="featured-event__card">
+		    <a href="#">
+				<div class="featured-event__image">
+					<img src="http://via.placeholder.com/350x150"/>
+				</div>
+				<div class="featured-event__content">
+				    <h2>Nyarlathotep... the crawling chaos... </h2>
+					<p>I do not recall distinctly when it began, but it was months ago. The general tension was horrible. To a season of political and social upheaval was added a strange and brooding apprehension of hideous physical danger; a danger widespread and all-embracing, such a danger as may be imagined only in the most terrible phantasms of the night.</p>
+				</div>
+				<div class="featured-event__footer">
+					<a href="#">
+					    <span class="featured-event__dagger">†</span> Read more...
+					</a>
+				</div>
+			</a>
 		</div>
-
 	</div>
 	<!--<?php dynamic_sidebar( 'sidebar-1' ); ?>-->
 </aside><!-- #secondary -->
